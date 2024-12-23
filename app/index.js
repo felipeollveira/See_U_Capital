206db5f117f9d46bf171d68b75747a440b3bf812
OLIVEIRA <FOLIV132@ford.com>
2024-12-23 11:30:03 -0300

    fix: add path path html and css = used to fix: path static /public

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 const port = process.env.PORT || 3000; // usa variael de ambiente se tiver
+
+app.use(express.static(__dirname + '/public'));
 
 
 const router = require('./root/router');
-
 app.use('/', router); 
 
 // Inicia o servidor
